953b8963860b40c8468f29b65d018f17bb9052c7
Jakub Głusiński <kuba.g777@gmail.com>
2024-05-09 21:11:07 +0200

    Podstawy rejestracji

--- frontend/src/routes/listView.tsx
+++ frontend/src/routes/listView.tsx
@@ -3,49 +3,40 @@ import { useParams } from "react-router-dom";
 import '../css/listView.css';
 
 const ListView = () => {
-    const [listId, setListId] = useState<string | null>(null);
 
     const { id } = useParams(); // odczytanie ID
 
-    useEffect(() => {
-        const urlParams = new URLSearchParams(window.location.search);
-        const id = urlParams.get('id');
-        if (id) {
-            setListId(id);
-        }
-    }, []);
+    // const deleteList = () => {
+    //     // Przeniesienie na stronę list
+    //     window.location.href = '/lists';
+    //     const data = new FormData();
+    //     if (id) {
+    //         data.append('list_id', id);
+    //         postData("/deleteList", data);
+    //     }
+    // };
 
-    const deleteList = () => {
-        // Przeniesienie na stronę list
-        window.location.href = '/lists';
-        const data = new FormData();
-        if (listId) {
-            data.append('list_id', listId);
-            postData("/deleteList", data);
-        }
-    };
+    // const postData = (url: string, data: FormData) => {
+    //     const options: RequestInit = {
+    //         method: 'POST',
+    //         body: data,
+    //     };
 
-    const postData = (url: string, data: FormData) => {
-        const options: RequestInit = {
-            method: 'POST',
-            body: data,
-        };
-
-        fetch(url, options)
-            .then(response => {
-                console.log(response.status);
-                window.location.reload();
-                return response.text();
-            })
-            .catch(error => console.error('Error:', error));
-    };
+    //     fetch(url, options)
+    //         .then(response => {
+    //             console.log(response.status);
+    //             //window.location.reload();
+    //             return response.text();
+    //         })
+    //         .catch(error => console.error('Error:', error));
+    // };
 
     const addNewField = (e: React.FormEvent) => {
         e.preventDefault();
         const data = new FormData(e.target as HTMLFormElement);
-        if (listId) {
-            data.append('list_id', listId);
-            postData("/addNewField", data);
+        if (id) {
+            data.append('list_id', id);
+            //postData("/addNewField", data);
         }
     };
 
@@ -57,18 +48,18 @@ const ListView = () => {
     };
 
     useEffect(() => {
-        if (listId) {
+        if (id) {
             const data = new FormData();
-            data.append('field_id', listId);
-            postData("/changeFieldState", data);
+            data.append('field_id', id);
+            //postData("/changeFieldState", data);
         }
-    }, [listId]);
-
+    }, [id]);
+    console.log(id);
     return (
         <div>
             <div className="mainFrame">
                 <div className="header">
-                    <button className="deleteList" type="button" onClick={deleteList}>Usuń listę</button>
+                    <button className="deleteList" type="button" /*onClick={deleteList}*/>Usuń listę</button>
                     <span>Nazwa listy ID: {id}</span>
                     <button className="addFieldButton" type="button" onClick={addNewFieldWnd}><div className="addNew">+</div></button>
                 </div>
